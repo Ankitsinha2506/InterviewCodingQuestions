fda8cfa9162ed937db860251a86a38869d90fe69
Ankit8210 <ankitsinha25061909@gmail.com>
2024-04-28 14:37:22 +0530

    EvenOddChecker

---
+++ InterviewCodingQuestions/src/com/interview/codingquestions/EvenOddChecker.java
@@ -0,0 +1,25 @@
+package com.interview.codingquestions;
+
+import java.util.Scanner;
+
+// Java Program to Check a Number is Odd or Even.
+public class EvenOddChecker {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number to check weather number is Even or Odd.");
+		int num = sc.nextInt();
+		
+		if (isEven(num)) {
+			System.out.println(+num + " is Even Number");
+		}
+		else {
+			System.out.println(num + " is Odd Number");
+		}
+		
+	}
+
+	private static boolean isEven(int num) {
+		return num % 2 == 0;
+	}
+
+}
