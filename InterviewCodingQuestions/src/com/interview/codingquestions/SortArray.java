17c04887a2419a15581367e538424a62534d02c3
Ankit8210 <ankitsinha25061909@gmail.com>
2024-05-12 16:23:30 +0530

    BubbleSort

---
+++ InterviewCodingQuestions/src/com/interview/codingquestions/SortArray.java
@@ -0,0 +1,15 @@
+package com.interview.codingquestions;
+
+public class SortArray {
+	public static void main(String[] args) {
+		// initialize Array.
+		int[] arr = new int[] {45,78,12,45,68,78,5,76,3,8,9};
+		
+		// sort the Array in Ascending order.
+		for(int i = 0; i < arr.length; i++)
+		{
+			
+		}
+	}
+
+}
